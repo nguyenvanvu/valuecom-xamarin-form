e712d17219a0d491fd9803e709c8ed1933c3a556
unknown-user <mr.vunguyen.it>
2015-07-24 08:52:39 +0700

    test commit xamarin

--- EmployeeDirectory.Android/LoginActivity.cs
+++ EmployeeDirectory.Android/LoginActivity.cs
@@ -42,7 +42,7 @@ namespace EmployeeDirectory.Android
         protected override void OnCreate (Bundle bundle)
         {
             base.OnCreate (bundle);
-
+			//
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Login);
 
